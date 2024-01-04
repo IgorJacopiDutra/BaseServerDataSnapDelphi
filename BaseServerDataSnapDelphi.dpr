22df7403601b7da1e92e8b91dbb1059edbafaac9
igorjacopidutranewcentury <igorjacopidutra@gmail.com>
2024-01-04 15:45:25 -0300

    Change name project
    Change name from ServerDataSnapDelphi to BaseServerDataSnapDelphi

---
+++ BaseServerDataSnapDelphi.dpr
@@ -0,0 +1,32 @@
+program BaseServerDataSnapDelphi;
+{$APPTYPE GUI}
+
+{$R *.dres}
+
+uses
+  Vcl.Forms,
+  Web.WebReq,
+  IdHTTPWebBrokerBridge,
+  frmPrincipal in 'view\frmPrincipal.pas' {Form1},
+  uServerMethods in 'controller\uServerMethods.pas',
+  uWebModule in 'model\uWebModule.pas' {WebModule1: TWebModule},
+  uConnectionDao in 'dao\uConnectionDao.pas',
+  uTools in 'model\uTools.pas',
+  uLogger in 'model\uLogger.pas',
+  uUserControl in 'controller\uUserControl.pas',
+  uUserModel in 'model\uUserModel.pas',
+  uUserDao in 'dao\uUserDao.pas',
+  uSystem.JSONUtil in 'model\uSystem.JSONUtil.pas',
+  uFileControl in 'controller\uFileControl.pas',
+  uFileModel in 'model\uFileModel.pas',
+  uFileDao in 'dao\uFileDao.pas';
+
+{$R *.res}
+
+begin
+  if WebRequestHandler <> nil then
+    WebRequestHandler.WebModuleClass := WebModuleClass;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
